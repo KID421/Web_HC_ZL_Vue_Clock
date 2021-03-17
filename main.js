49ddb0a74a151b7912074918310fb1f631f2c55e
KID421 <suchild7@gmail.com>
2021-03-17 19:30:21 +0800

    設定 Vue 資料並測試

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("哈囉~");
+// 常數 時鐘 = { 資料 () {} }
+const clock = {
+    data() {
+        return {
+            test: 19
+        }
+    }
+};
+
+// Vue 建立 APP(常數時鐘).安裝("ID 時鐘")
+Vue.createApp(clock).mount("#clock");
