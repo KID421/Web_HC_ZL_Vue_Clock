4e2b5bf8660c7703cc7eebb6421cdf19fecca403
KID421 <suchild7@gmail.com>
2021-03-17 20:03:24 +0800

    完成時鐘效果

--- main.js
+++ main.js
@@ -11,11 +11,16 @@ const clock = {
         // 更新時鐘
         updateClock() {
             var date = new Date();
-            console.log(date);
+            // console.log(date);
+            // 更新 小時、分鐘、秒數
+            this.h = date.getHours();
+            this.m = date.getMinutes();
+            this.s = date.getSeconds();
         }
     },
     mounted() {
-        this.updateClock();
+        // 設定間隔(函式，間隔)
+        setInterval(this.updateClock, 1000);
     }
 };
 
